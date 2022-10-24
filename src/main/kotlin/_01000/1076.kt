72f4bfc4fd9cd9b23335b118ddebc659894f39b2
Colabear754 <jky2406@gmail.com>
2022-10-24 11:06:17 +0900

    저항

---
+++ src/main/kotlin/_01000/1076.kt
@@ -0,0 +1,36 @@
+package _01000
+
+fun main() {
+    val first = readln()
+    val second = readln()
+    val third = readln()
+    println(add(first).plus(add(second)).toLong() * mul(third))
+}
+
+fun add(string: String):String = when (string) {
+    "black" -> "0"
+    "brown" -> "1"
+    "red" -> "2"
+    "orange" -> "3"
+    "yellow" -> "4"
+    "green" -> "5"
+    "blue" -> "6"
+    "violet" -> "7"
+    "grey" -> "8"
+    "white" -> "9"
+    else -> "0"
+}
+
+fun mul(string: String):Long = when (string) {
+    "black" -> 1
+    "brown" -> 10
+    "red" -> 100
+    "orange" -> 1000
+    "yellow" -> 10000
+    "green" -> 100000
+    "blue" -> 1000000
+    "violet" -> 10000000
+    "grey" -> 100000000
+    "white" -> 1000000000
+    else -> 1
+}
