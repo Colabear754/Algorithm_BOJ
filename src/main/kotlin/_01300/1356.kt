8c5bba744720f72e44d3a5ffe75dc02d47a78f63
Colabear754 <jky2406@gmail.com>
2022-11-08 15:32:41 +0900

    유진수

--- src/main/kotlin/_01300/1356.kt
+++ src/main/kotlin/_01300/1356.kt
@@ -2,6 +2,7 @@ package _01300
 
 fun main() {
     val n = readln()
+    // 한자리 수는 유진수가 될 수 없음
     if (n.length == 1) {
         println("NO")
         return
@@ -12,18 +13,21 @@ fun main() {
         val y = n.substring(i + 1, n.length)
         var X = 1
         var Y = 1
+        // 앞 수의 곱 계산
         for (j in x.indices) {
             X *= x[j] - '0'
         }
+        // 뒷 수의 곱 계산
         for (j in y.indices) {
             Y *= y[j] - '0'
         }
 
+        // 한번이라도 유진수를 만족하는 조건이 있으면 YES를 출력하고 종료
         if (X == Y) {
             println("YES")
             return
         }
     }
-
+    // 반복문 도중에 프로그램이 종료되지 않으면 유진수가 아니므로 NO를 출력
     println("NO")
 }
