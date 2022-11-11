9e0cc7f05b4f3a1c0c60f2b4abb5d32e940fa41e
Colabear754 <jky2406@gmail.com>
2022-11-11 16:39:11 +0900

    꼬리를 무는 숫자 나열

---
+++ src/main/kotlin/_01500/1598.kt
@@ -0,0 +1,6 @@
+package _01500
+
+import kotlin.math.abs
+
+fun position(n: Int): Array<Int> = if (n % 4 == 0) arrayOf(n / 4 - 1, 3) else arrayOf(n / 4, n % 4 - 1) // 숫자의 좌표
+fun main() = print(readln().split(" ").map { it.toInt() }.let { abs(position(it[0])[0] - position(it[1])[0]) + abs(position(it[0])[1] - position(it[1])[1]) })
