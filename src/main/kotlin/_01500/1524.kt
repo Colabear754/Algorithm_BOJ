3bd68cba3d893d5ea0b0df2ecb915641e21a0d50
Colabear754 <jky2406@gmail.com>
2022-11-11 10:15:16 +0900

    세준세비
    가장 강한 병사는 마지막까지 죽지 않으므로, 세준과 세비의 병사 중 가장 강한 병사들의 힘을 비교하면 된다.
    문제의 조건 상 무승부인 C는 발생할 수 없다.
    리스트를 정렬하여 하는 방식에서 최대값만 구해서 비교하는 방식으로 수정.
    실제로는 그냥 리스트의 max() 메소드를 사용하면 되지만 백준에서는 컴파일 에러를 발생시킨다.

--- src/main/kotlin/_01500/1524.kt
+++ src/main/kotlin/_01500/1524.kt
@@ -1,16 +1,26 @@
 package _01500
 
+import kotlin.math.max
+
 fun main() {
     val bw = System.out.bufferedWriter()
     val n = readln().toInt()
     repeat(n) {
         readln()
         readln()
-        val S = readln().split(" ").map { it.toInt() }.sortedDescending().toMutableList()
-        val B = readln().split(" ").map { it.toInt() }.sortedDescending().toMutableList()
+        val S = readln().split(" ").map { it.toInt() }
+        val B = readln().split(" ").map { it.toInt() }
+        var SM = 0
+        var BM = 0
+        for (s in S) {
+            SM = max(SM, s)
+        }
+        for (b in B) {
+            BM = max(BM, b)
+        }
         bw.write(
             when {
-                S[0] >= B[0] -> "S\n"
+                SM >= BM -> "S\n"
                 else -> "B\n"
             }
         )
