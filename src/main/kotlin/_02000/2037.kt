0f27bfd0503a74a066e3a62849cbea765d44d82c
Colabear754 <jky2406@gmail.com>
2022-11-18 11:11:02 +0900

    문자메시지

---
+++ src/main/kotlin/_02000/2037.kt
@@ -0,0 +1,43 @@
+package _02000
+
+val chars = arrayOf(
+    arrayOf('A', 'B', 'C'),
+    arrayOf('D', 'E', 'F'),
+    arrayOf('G', 'H', 'I'),
+    arrayOf('J', 'K', 'L'),
+    arrayOf('M', 'N', 'O'),
+    arrayOf('P', 'Q', 'R', 'S'),
+    arrayOf('T', 'U', 'V'),
+    arrayOf('W', 'X', 'Y', 'Z')
+)
+
+fun main() = with(System.`in`.bufferedReader()) {
+    val (p, w) = readLine().split(" ").map { it.toInt() }
+    val str = readLine()
+    var sec = addedVal(str, 0) * p
+    for (i in 1..str.lastIndex) {
+        if (getNum(str[i]) == 8) {
+            sec += p
+        } else {
+            sec += addedVal(str, i) * p
+            if (getNum(str[i - 1]) == getNum(str[i])) {
+                sec += w
+            }
+        }
+    }
+    println(sec)
+}
+
+fun addedVal(s: String, i: Int) = chars[getNum(s[i])].indexOf(s[i]) + 1
+
+fun getNum(c: Char) = when (c) {
+    in 'A'..'C' -> 0
+    in 'D'..'F' -> 1
+    in 'G'..'I' -> 2
+    in 'J'..'L' -> 3
+    in 'M'..'O' -> 4
+    in 'P'..'S' -> 5
+    in 'T'..'V' -> 6
+    in 'W'..'Z' -> 7
+    else -> 8
+}
