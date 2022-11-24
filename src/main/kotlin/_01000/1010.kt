d76e4f4dc849528d63ff3c1049b1fee4460769d3
Colabear754 <jky2406@gmail.com>
2022-11-24 11:20:12 +0900

    다리 놓기

--- src/main/kotlin/_01000/1010.kt
+++ src/main/kotlin/_01000/1010.kt
@@ -6,22 +6,21 @@ fun main() = with(System.`in`.bufferedReader()) {
     val bw = System.out.bufferedWriter()
     val times = readLine().toInt()
     repeat(times) {
-        bw.write("${readLine().split(" ").map { it.toInt() }.let { it[1].comb(it[0]) }}\n")
+        bw.write("${readLine().split(" ").map { it.toInt() }.let { comb(it[1], it[0]) }}\n")
     }
     bw.close()
 }
 
-private fun Int.comb(n: Int): BigInteger {
-    var a = this
-    var b = n
-    var res = BigInteger.ONE
-    b = minOf(b, a - b)
-    var bFac = 1L
-    for (i in 2..b) {
-        bFac *= i.toLong()
+fun comb(n: Int, k: Int): BigInteger {
+    val a = minOf(k, n - k)
+    val b = maxOf(k, n - k)
+    var result = 1.toBigInteger()
+    var minFac = 1L
+    for (i in n downTo b + 1) {
+        result = result.multiply(i.toBigInteger())
     }
-    while (b-- > 0) {
-        res = res.multiply(a--.toBigInteger())
+    for (i in 2..a) {
+        minFac *= i
     }
-    return res.divide(bFac.toBigInteger())
+    return result.divide(minFac.toBigInteger())
 }
