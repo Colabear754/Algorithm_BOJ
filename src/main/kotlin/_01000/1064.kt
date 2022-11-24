04c09c75323457056fd090a61586e1ff02281055
Colabear754 <jky2406@gmail.com>
2022-11-24 14:15:33 +0900

    평행사변형

---
+++ src/main/kotlin/_01000/1064.kt
@@ -0,0 +1,23 @@
+package _01000
+
+import kotlin.math.abs
+import kotlin.math.pow
+import kotlin.math.sqrt
+
+fun main() = with(System.`in`.bufferedReader()) {
+    val input = readLine().split(" ").map { it.toDouble() }
+    val p1 = Point(input[0], input[1])
+    val p2 = Point(input[2], input[3])
+    val p3 = Point(input[4], input[5])
+
+    if (abs((p3.y - p2.y) / (p3.x - p2.x)) == abs((p2.y - p1.y) / (p2.x - p1.x))) {
+        println(-1)
+        return@with
+    }
+
+    val sides = arrayOf(distance(p1, p2), distance(p1, p3), distance(p2, p3)).sortedArray()
+    println((sides[2] - sides[0]) * 2)
+}
+
+fun distance(p1: Point, p2: Point) = sqrt((p1.x - p2.x).pow(2) + (p1.y - p2.y).pow(2))
+class Point(val x: Double, val y: Double)
