4ca53d124e440ada42ad2bc5bf4eb0128c6fb1af
Colabear754 <jky2406@gmail.com>
2022-11-24 16:24:10 +0900

    그룹 단어 체커

--- src/main/kotlin/_01300/1316.kt
+++ src/main/kotlin/_01300/1316.kt
@@ -5,17 +5,13 @@ fun main() = with(System.`in`.bufferedReader()) {
     var cnt = 0
     case@ for (i in 1..n) {
         val str = readLine()
-        val check = BooleanArray(26)    // 알파벳이 나온적이 있는지 체크
+        val check = BooleanArray(26)
         check[str[0] - 'a'] = true
         for (j in 1..str.lastIndex) {
-            // 이전 문자와 현재 문자가 다르고
-            if (str[j] != str[j - 1]) {
-                // 현재 문자가 나온적이 있으면 다음 케이스로 넘어감
-                if (check[str[j] - 'a']) {
-                    continue@case
-                } else {
-                    check[str[j] - 'a'] = true
-                }
+            if (!check[str[j] - 'a']) {
+                check[str[j] - 'a'] = true
+            } else if (str[j] != str[j - 1]) {
+                continue@case
             }
         }
         cnt++
