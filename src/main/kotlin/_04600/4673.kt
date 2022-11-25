1fcaaee98bfa8cff0d18be98fa97120e5057fc90
Colabear754 <jky2406@gmail.com>
2022-11-25 16:42:10 +0900

    셀프 넘버

---
+++ src/main/kotlin/_04600/4673.kt
@@ -0,0 +1,27 @@
+package _04600
+
+fun main() = with(System.out.bufferedWriter()) {
+    val check = BooleanArray(10001)
+
+    for (i in 1..10000) {
+        if (d(i) <= 10000) {
+            check[d(i)] = true
+        }
+    }
+    for (i in 1..10000) {
+        if (!check[i]) write("$i\n")
+    }
+    close()
+}
+
+fun d(n: Int): Int {
+    var sum = n
+    var num = n
+
+    while (num != 0) {
+        sum += num % 10
+        num /= 10
+    }
+
+    return sum
+}
