6df7465bcf6c335057b0434487d1e648ce8e7266
Colabear754 <jky2406@gmail.com>
2022-11-28 15:02:48 +0900

    크로아티아 알파벳

---
+++ src/main/kotlin/_02900/2941.kt
@@ -0,0 +1,3 @@
+package _02900
+
+fun main() = print(readln().replace("c=", "?").replace("c-", "?").replace("dz=", "?").replace("d-", "?").replace("lj", "?").replace("nj", "?").replace("s=", "?").replace("z=", "?").length)
