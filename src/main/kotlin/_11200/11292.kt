99aed71146b5ca1c74a2fe11de0d5acaccf54864
Colabear754 <jky2406@gmail.com>
2022-11-28 12:54:36 +0900

    키 큰 사람

---
+++ src/main/kotlin/_11200/11292.kt
@@ -0,0 +1,22 @@
+package _11200
+
+import java.util.StringTokenizer
+
+fun main() = with(System.`in`.bufferedReader()) {
+    var c: Int
+    val bw = System.out.bufferedWriter()
+    while (readLine().also { c = it.toInt() } != "0") {
+        var max = 0.0
+        val list = ArrayList<Person>()
+        repeat(c) {
+            val st = StringTokenizer(readLine())
+            list.add(Person(st.nextToken(), st.nextToken().toDouble().also { max = maxOf(max, it) }))
+        }
+        bw.write("${list.filter { it.height == max }.joinToString(" ")}\n")
+    }
+    bw.close()
+}
+
+class Person(val name: String, val height: Double) {
+    override fun toString() = this.name
+}
