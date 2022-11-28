c55754a04924fb7e33c316bf1ccb1be12002a1ad
Colabear754 <jky2406@gmail.com>
2022-11-28 16:16:07 +0900

    설탕 배달

---
+++ src/main/kotlin/_02800/2839.kt
@@ -0,0 +1,17 @@
+package _02800
+
+fun main() = with(System.`in`.bufferedReader()) {
+    val n = readLine().toInt()
+    var sugar = n / 5
+    var rest = n % 5
+    while (rest in 1..n) {
+        if (rest % 3 != 0) {
+            sugar--
+            rest += 5
+        } else {
+            sugar += rest / 3
+            rest = 0
+        }
+    }
+    println(if (rest > n) -1 else sugar)
+}
