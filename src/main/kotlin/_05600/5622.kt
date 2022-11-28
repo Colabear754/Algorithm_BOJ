4eecf3f53b48daae1a0ebbe856154f4d05b2feaf
Colabear754 <jky2406@gmail.com>
2022-11-28 15:56:14 +0900

    다이얼

---
+++ src/main/kotlin/_05600/5622.kt
@@ -0,0 +1,22 @@
+package _05600
+
+fun main() = with(System.`in`.bufferedReader()) {
+    val str = readLine()
+    var sec = 0
+    for (c in str) {
+        sec += getSec(c)
+    }
+    println(sec)
+}
+
+fun getSec(c: Char) = when (c) {
+    in 'A'..'C' -> 3
+    in 'D'..'F' -> 4
+    in 'G'..'I' -> 5
+    in 'J'..'L' -> 6
+    in 'M'..'O' -> 7
+    in 'P'..'S' -> 8
+    in 'T'..'V' -> 9
+    in 'W'..'Z' -> 10
+    else -> 11
+}
