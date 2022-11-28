bcfb7ee0d386575c67076982c51926be3f9d701d
Colabear754 <jky2406@gmail.com>
2022-11-28 16:22:08 +0900

    설탕 배달

--- src/main/kotlin/_02800/2839.kt
+++ src/main/kotlin/_02800/2839.kt
@@ -2,19 +2,19 @@ package _02800
 
 fun main() = with(System.`in`.bufferedReader()) {
     val n = readLine().toInt()
-    var mod = n
+    var rest = n
     var sugar = n / 5
-    mod %= 5
-    while (mod in 1..n) {
-        if (mod % 3 != 0) {
+    rest %= 5
+    while (rest in 1..n) {
+        if (rest % 3 != 0) {
             sugar--
-            mod += 5
+            rest += 5
         } else {
-            sugar += mod / 3
-            mod = 0
+            sugar += rest / 3
+            rest = 0
         }
     }
-    if (mod > 0) {
+    if (rest > 0) {
         sugar = -1
     }
     println(sugar)
