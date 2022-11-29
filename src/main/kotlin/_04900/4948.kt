9aea8a122d3b9f82c1f8e8e7e72b143a8cc2c1e1
Colabear754 <jky2406@gmail.com>
2022-11-29 14:25:43 +0900

    베르트랑 공준

--- src/main/kotlin/_04900/4948.kt
+++ src/main/kotlin/_04900/4948.kt
@@ -1,21 +1,22 @@
 package _04900
 
+import kotlin.math.sqrt
+
 fun main() = with(System.`in`.bufferedReader()) {
     val bw = System.out.bufferedWriter()
     val cases = ArrayList<Int>()
     while (readLine().also { cases.add(it.toInt()) } != "0") {}
     cases.remove(0)
-    val max = cases.maxOrNull()!! * 2
+    val max = 123456 * 2
     val isPrime = BooleanArray(max + 1)
     isPrime[0] = true
     isPrime[1] = true
-    var i = 2
-    while (i * i <= max) {
+    val sqrt = sqrt(max.toDouble()).toInt()
+    for (i in 2..sqrt) {
         if (!isPrime[i]) {
             for (j in i * i..max step i)
                 isPrime[j] = true
         }
-        i++
     }
     for (t in cases) {
         bw.write("${isPrime.slice(t + 1..2 * t).count { !it }}\n")
