839ac9e825ab944475e98930025f70e84d0f2001
Colabear754 <jky2406@gmail.com>
2022-11-29 16:12:03 +0900

    골드바흐의 추측

--- src/main/kotlin/_09000/9020.kt
+++ src/main/kotlin/_09000/9020.kt
@@ -5,12 +5,11 @@ import kotlin.math.sqrt
 fun main() = with(System.`in`.bufferedReader()) {
     val bw = System.out.bufferedWriter()
     val cases = ArrayList<Int>()
-
+    var max = 0
     repeat(readLine().toInt()) {
-        cases.add(readLine().toInt())
+        cases.add(readLine().toInt().also { max = maxOf(max, it) })
     }
 
-    val max = cases.maxOrNull()!!
     val isPrime = BooleanArray(max + 1)
     isPrime[0] = true
     isPrime[1] = true
@@ -23,7 +22,7 @@ fun main() = with(System.`in`.bufferedReader()) {
     }
 
     for (t in cases) {
-        for (i in t / 2..t) {
+        for (i in t / 2..t - 2) {
             if (!isPrime[i] && !isPrime[t - i]) {
                 bw.write("${t - i} $i\n")
                 break
