fa3ac2acb1d8eabf5f4f9ef844a388790f5ff085
Colabear754 <jky2406@gmail.com>
2022-11-30 19:43:22 +0900

    나이순 정렬

---
+++ src/main/kotlin/_10800/10814.kt
@@ -0,0 +1,22 @@
+package _10800
+
+import java.util.PriorityQueue
+import java.util.StringTokenizer
+
+fun main() = with(System.`in`.bufferedReader()) {
+    val bw = System.out.bufferedWriter()
+    val member = PriorityQueue<Person>()
+    repeat(readLine().toInt()) {
+        val st = StringTokenizer(readLine())
+        member.add(Person(st.nextToken().toInt(), st.nextToken(), it))
+    }
+    repeat(member.size) {
+        bw.write("${member.poll()}\n")
+    }
+    bw.close()
+}
+
+class Person(val age: Int, val name: String, val order: Int): Comparable<Person> {
+    override fun toString() = "$age $name"
+    override fun compareTo(other: Person) = if (this.age - other.age != 0) this.age - other.age else this.order - other.order
+}
