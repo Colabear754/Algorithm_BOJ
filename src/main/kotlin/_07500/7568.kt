e8f9728735ba687d82d6c01fbc6d98ae022152a8
Colabear754 <jky2406@gmail.com>
2022-12-02 13:28:35 +0900

    덩치

---
+++ src/main/kotlin/_07500/7568.kt
@@ -0,0 +1,23 @@
+package _07500
+
+import java.util.StringTokenizer
+
+fun main() = with(System.`in`.bufferedReader()) {
+    val sb = StringBuilder("")
+    val people = Array(readLine().toInt()) {
+        val st = StringTokenizer(readLine())
+        Person(st.nextToken().toInt(), st.nextToken().toInt())
+    }
+    for (i in people.indices) {
+        var rank = 1
+        for (j in people.indices) {
+            rank += if (people[i] < people[j]) 1 else 0
+        }
+        sb.append("$rank ")
+    }
+    println(sb)
+}
+
+class Person(val w: Int, val h: Int) : Comparable<Person> {
+    override fun compareTo(other: Person) = if ((this.w - other.w) * (this.h - other.h) > 0) this.w - other.w else 0
+}
