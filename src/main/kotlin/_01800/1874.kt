5825315c72da6d249366070b42a11d99928759bc
Colabear754 <jky2406@gmail.com>
2022-12-02 15:39:33 +0900

    스택 수열

--- src/main/kotlin/_01800/1874.kt
+++ src/main/kotlin/_01800/1874.kt
@@ -3,22 +3,22 @@ package _01800
 import java.util.Stack
 
 fun main() = with(System.`in`.bufferedReader()) {
-    val bw = System.out.bufferedWriter()
+    val sb = StringBuilder()
     val stack = Stack<Int>()
     val arr = IntArray(readLine().toInt()) { readLine().toInt() }
     var curr = 1
     for (num in arr) {
         while (curr <= num) {
             stack.push(curr++)
-            bw.write("+\n")
+            sb.append("+\n")
         }
         if (stack.peek() == num) {
             stack.pop()
-            bw.write("-\n")
+            sb.append("-\n")
         } else {
             println("NO")
             return@with
         }
     }
-    bw.close()
+    println(sb)
 }
