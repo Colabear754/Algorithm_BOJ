c628da067ac71278c4edb52ae20f20817a39e861
Colabear754 <jky2406@gmail.com>
2022-12-02 14:18:25 +0900

    통계학

--- src/main/kotlin/_02100/2108.kt
+++ src/main/kotlin/_02100/2108.kt
@@ -7,13 +7,15 @@ fun main() = with(System.`in`.bufferedReader()) {
     val numbers = IntArray(readLine().toInt())
     val cnt = IntArray(8001)
     var n: Int
+    var avg = 0.0
     for (i in numbers.indices) {
         numbers[i] = readLine().toInt().also { n = it }
         cnt[n + 4000]++
+        avg += n
     }
     if (cnt.count { it == cnt.maxOrNull() } >= 2) cnt[cnt.indexOf(cnt.maxOrNull()!!)]--
     numbers.sort()
-    bw.write("${(numbers.sum().toDouble() / numbers.size).roundToInt()}\n")
+    bw.write("${(avg / numbers.size).roundToInt()}\n")
     bw.write("${numbers[numbers.size / 2]}\n")
     bw.write("${cnt.indexOf(cnt.maxOrNull()!!) - 4000}\n")
     bw.write("${numbers.last() - numbers[0]}")
