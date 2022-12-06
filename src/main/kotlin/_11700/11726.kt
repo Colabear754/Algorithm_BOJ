4c6d0baa850a5a682b69010ff1e0abf897242393
Colabear754 <jky2406@gmail.com>
2022-12-06 14:54:25 +0900

    2×n 타일링

---
+++ src/main/kotlin/_11700/11726.kt
@@ -0,0 +1,11 @@
+package _11700
+
+fun main() = with(System.`in`.bufferedReader()) {
+    val n = readLine().toInt()
+    val cases = IntArray(n + 1)
+    cases[0] = 1.also { cases[1] = it }
+    for (i in 2..n) {
+        cases[i] = (cases[i - 1] + cases[i - 2]) % 10007
+    }
+    println(cases[n])
+}
