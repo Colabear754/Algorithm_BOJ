aaef9dd80a8718028d0baa91d2c9434d02f620b8
Colabear754 <jky2406@gmail.com>
2022-12-07 08:51:52 +0900

    색종이

--- src/main/kotlin/_02500/2563.kt
+++ src/main/kotlin/_02500/2563.kt
@@ -1,12 +1,8 @@
 package _02500
 
 fun main() = with(System.`in`.bufferedReader()) {
-    var maxX = 0
-    var maxY = 0
-    val paper = Array(readLine().toInt()) {
-        readLine().split(' ').map { it.toInt() }.also { maxX = maxOf(it[0], maxX); maxY = maxOf(it[1], maxY) }
-    }
-    val field = Array(maxX + 10) { BooleanArray(maxY + 10) }
+    val paper = Array(readLine().toInt()) { readLine().split(' ').map { it.toInt() } }
+    val field = Array(100) { BooleanArray(100) }
     repeat(paper.size) {
         for (x in paper[it][0] until paper[it][0] + 10)
             for (y in paper[it][1] until paper[it][1] + 10)
