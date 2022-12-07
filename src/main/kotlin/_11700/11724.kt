ab70450d0293df32a14b00e9b9a660bdf914c5b0
Colabear754 <jky2406@gmail.com>
2022-12-07 15:36:40 +0900

    연결 요소의 개수

---
+++ src/main/kotlin/_11700/11724.kt
@@ -0,0 +1,31 @@
+package _11700
+
+val graph = Array(1001) { ArrayList<Int>() }
+val visited = BooleanArray(1001)
+
+fun main() = with(System.`in`.bufferedReader()) {
+    val (n, m) = readLine().split(' ').map { it.toInt() }
+    var result = 0
+    repeat(m) {
+        readLine().split(' ').map { it.toInt() }.let {
+            graph[it[0]].add(it[1])
+            graph[it[1]].add(it[0])
+        }
+    }
+    repeat(n) {
+        if (!visited[it + 1]) {
+            dfs(it + 1)
+            result++
+        }
+    }
+    println(result)
+}
+
+fun dfs(cur: Int) {
+    for (n in graph[cur]) {
+        if (!visited[n]) {
+            visited[n] = true
+            dfs(n)
+        }
+    }
+}
