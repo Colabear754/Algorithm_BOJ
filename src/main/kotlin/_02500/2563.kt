1e03f0fb5d846d0a7d8313672a48fdb47fed267d
Colabear754 <jky2406@gmail.com>
2022-12-07 09:15:45 +0900

    색종이

--- src/main/kotlin/_02500/2563.kt
+++ src/main/kotlin/_02500/2563.kt
@@ -1,12 +1,13 @@
 package _02500
 
 fun main() = with(System.`in`.bufferedReader()) {
-    val paper = Array(readLine().toInt()) { readLine().split(' ').map { it.toInt() } }
     val field = Array(100) { BooleanArray(100) }
-    repeat(paper.size) {
-        for (x in paper[it][0] until paper[it][0] + 10)
-            for (y in paper[it][1] until paper[it][1] + 10)
-                field[x][y] = true
+    repeat(readLine().toInt()) {
+        readLine().split(' ').map { it.toInt() }.let { p ->
+            for (x in p[0] until p[0] + 10)
+                for (y in p[1] until p[1] + 10)
+                    field[x][y] = true
+        }
     }
     println(field.sumOf { it.count { p -> p } })
 }
