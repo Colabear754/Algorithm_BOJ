6d45e23469274219be8310a6b28b0fc200623169
Colabear754 <jky2406@gmail.com>
2022-12-17 00:36:13 +0900

    구간 합 구하기 4

--- src/main/kotlin/_11600/11659.kt
+++ src/main/kotlin/_11600/11659.kt
@@ -5,11 +5,10 @@ import java.util.StringTokenizer
 fun main() = with(System.`in`.bufferedReader()) {
     val bw = System.out.bufferedWriter()
     val (n, m) = readLine().split(' ').map { it.toInt() }
-    val arr = IntArray(n + 1)
     val sum = IntArray(n + 1)
     val st = StringTokenizer(readLine())
     repeat(n) { i ->
-        arr[i + 1] = st.nextToken().toInt().also { n -> sum[i + 1] = sum[i] + n }
+        sum[i + 1] = sum[i] + st.nextToken().toInt()
     }
     repeat(m) {
         bw.write(StringTokenizer(readLine()).run { "${-sum[nextToken().toInt() - 1] + sum[nextToken().toInt()]}\n" })
