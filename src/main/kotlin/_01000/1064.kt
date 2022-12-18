91adfad0049cfa4ac7e1036b06a596c9b1825a62
Colabear754 <jky2406@gmail.com>
2022-12-18 13:05:18 +0900

    평행사변형

--- src/main/kotlin/_01000/1064.kt
+++ src/main/kotlin/_01000/1064.kt
@@ -1,6 +1,5 @@
 package _01000
 
-import kotlin.math.abs
 import kotlin.math.pow
 import kotlin.math.sqrt
 
@@ -9,8 +8,7 @@ fun main() = with(System.`in`.bufferedReader()) {
     val p1 = Point(input[0], input[1])
     val p2 = Point(input[2], input[3])
     val p3 = Point(input[4], input[5])
-    // 두 직선의 기울기가 같은 경우 = 평행사변형이 만들어지지 않으므로 -1을 출력
-    if (abs((p3.y - p2.y) / (p3.x - p2.x)) == abs((p2.y - p1.y) / (p2.x - p1.x))) {
+    if ((p3.y - p2.y) * (p2.x - p1.x) == (p2.y - p1.y) * (p3.x - p2.x)) {
         println(-1)
         return@with
     }
@@ -19,5 +17,5 @@ fun main() = with(System.`in`.bufferedReader()) {
     println((sides[2] - sides[0]) * 2)
 }
 
-fun distance(p1: Point, p2: Point) = sqrt((p1.x - p2.x).pow(2) + (p1.y - p2.y).pow(2))  // 두 점 사이의 거리
-class Point(val x: Double, val y: Double)   // 점의 좌표를 나타낼 클래스
+fun distance(p1: Point, p2: Point) = sqrt((p1.x - p2.x).pow(2) + (p1.y - p2.y).pow(2))
+class Point(val x: Double, val y: Double)
