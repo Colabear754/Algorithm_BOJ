1edc36823237864139294e102c5dea62407154c7
Colabear754 <jky2406@gmail.com>
2022-12-20 16:46:55 +0900

    등차 수열과 등비 수열

---
+++ src/main/kotlin/_14200/14277.kt
@@ -0,0 +1,17 @@
+package _14200
+
+fun main() = with(System.`in`.bufferedReader()) {
+    var (a, b, c, d, u) = readLine().split(' ').map { it.toLong() }
+    var count = 0L
+    if (a <= u) count = (u - a) / b + 1
+    if (d == 1L && c <= u) {
+        if (a != c && (a >= c || (c - a) % b != 0L)) count++
+    } else {
+        while (c <= u) {
+            count++
+            if (c >= a && (c - a) % b == 0L) count--
+            c *= d
+        }
+    }
+    println(count)
+}
