3883d8a298e1dcf00f6353c551d4004ad244d1cc
Colabear754 <jky2406@gmail.com>
2022-12-21 11:18:29 +0900

    인사성 밝은 곰곰이

---
+++ src/main/kotlin/_25100/25192.kt
@@ -0,0 +1,17 @@
+package _25100
+
+fun main() = with(System.`in`.bufferedReader()) {
+    val n = readLine().toInt()
+    var users = HashSet<String>()
+    var count = 0
+    repeat(n) {
+        readLine().also {
+            if (it == "ENTER") {
+                users = HashSet()
+                return@repeat
+            }
+            if (users.add(it)) count++
+        }
+    }
+    println(count)
+}
