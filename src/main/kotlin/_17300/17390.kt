341de1b4e0975931b3a55c798422cd0e047d0e1f
Colabear754 <jky2406@gmail.com>
2022-12-28 09:01:04 +0900

    이건 꼭 풀어야 해!

---
+++ src/main/kotlin/_17300/17390.kt
@@ -0,0 +1,19 @@
+package _17300
+
+import java.util.StringTokenizer
+
+fun main() = with(System.`in`.bufferedReader()) {
+    val bw = System.out.bufferedWriter()
+    val (n, q) = readLine().split(' ').map { it.toInt() }
+    val st = StringTokenizer(readLine())
+    val arr = IntArray(n) { st.nextToken().toInt() }.sortedArray()
+    val prefixSum = IntArray(n + 1)
+    for (i in 1..n) {
+        prefixSum[i] = prefixSum[i - 1] + arr[i - 1]
+    }
+    repeat(q) {
+        val (l, r) = readLine().split(' ').map { it.toInt() }
+        bw.write("${prefixSum[r] - prefixSum[l - 1]}\n")
+    }
+    bw.close()
+}
