fee0c1813c330e7e271727a7ed8d5cabf9746784
Colabear754 <jky2406@gmail.com>
2023-01-10 08:49:28 +0900

    가희와 프로세스 1

---
+++ src/main/kotlin/_21700/21773.kt
@@ -0,0 +1,29 @@
+package _21700
+
+import java.util.PriorityQueue
+import java.util.StringTokenizer
+
+fun main() = with(System.`in`.bufferedReader()) {
+    val bw = System.out.bufferedWriter()
+    val (t, n) = readLine().split(' ').map { it.toInt() }
+    val scheduler = PriorityQueue<Process>()
+    repeat(n) {
+        StringTokenizer(readLine()).apply { scheduler.add(Process(nextToken().toInt(), nextToken().toInt(), nextToken().toInt())) }
+    }
+    for (i in 1..t) {
+        if (scheduler.isEmpty()) break
+        val process = scheduler.poll().apply {
+            bw.write("$id\n")
+            time--
+            priority--
+        }
+
+        if (process.time > 0)
+            scheduler.add(process)
+    }
+    bw.close()
+}
+
+class Process(val id: Int, var time: Int, var priority: Int) : Comparable<Process> {
+    override fun compareTo(other: Process) = if (other.priority == this.priority) this.id - other.id else other.priority - this.priority
+}
