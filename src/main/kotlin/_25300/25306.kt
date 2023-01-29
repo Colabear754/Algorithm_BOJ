91e9d4a19a3bf931afe6d994679aed89fed36b38
Colabear754 <jky2406@gmail.com>
2023-01-29 16:19:09 +0900

    연속 XOR

---
+++ src/main/kotlin/_25300/25306.kt
@@ -0,0 +1,13 @@
+package _25300
+
+fun main() = with(System.`in`.bufferedReader()) {
+    val (a, b) = readLine().split(' ').map { it.toLong() }
+    print(b.cumulateXOR() xor (a - 1).cumulateXOR())
+}
+
+fun Long.cumulateXOR() = when (this % 4) {
+    0L -> this
+    1L -> 1
+    2L -> this + 1
+    else -> 0
+}
