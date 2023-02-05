fd6d9c7ca19c4dc92a9ec9a51c23d2576aecea89
Colabear754 <jky2406@gmail.com>
2023-02-05 20:21:03 +0900

    두 배 더하기

--- src/main/kotlin/_12900/12931.kt
+++ src/main/kotlin/_12900/12931.kt
@@ -4,31 +4,22 @@ import java.util.*
 
 fun main() = with(System.`in`.bufferedReader()) {
     val n = readLine().toInt()
-    var array = StringTokenizer(readLine()).run { IntArray(n) { nextToken().toInt() } }
+    val array = StringTokenizer(readLine()).run { IntArray(n) { nextToken().toInt() } }
     var count = 0
     while (true) {
-        var isClear = true
-        var isChanged = false
-        array = array.map {
-            if (it != 0) isClear = false
-            if (it % 2 == 1) {
-                isChanged = true
+        for (i in array.indices) {
+            if (array[i] % 2 == 1) {
                 count++
-                it - 1
-            } else {
-                it
+                array[i]--
             }
-        }.toIntArray()
-
-        if (isClear) break
-
-        if (!isChanged) {
-            array = array.map {
-                if (it != 0) isClear = false
-                it / 2
-            }.toIntArray()
-            count++
         }
+
+        if (array.count { it != 0 } == 0) break
+
+        for (i in array.indices) {
+            array[i] /= 2
+        }
+        count++
     }
     println(count)
 }
