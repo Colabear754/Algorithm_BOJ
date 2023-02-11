746a151a7f48539e7f0e9fa89e81b3465a4b6bc9
Colabear754 <jky2406@gmail.com>
2023-02-11 21:41:32 +0900

    :chino_shock:
    숏코딩으로 리팩토링

--- src/main/kotlin/_27300/27310.kt
+++ src/main/kotlin/_27300/27310.kt
@@ -1,10 +1,3 @@
 package _27300
 
-fun main() {
-    val emoji = readln()
-    var difficulty = emoji.length + 2
-    for (c in emoji) {
-        if (c == '_') difficulty += 5
-    }
-    println(difficulty)
-}
+fun main()=print(readln().let{it.length+2+it.count{c->c=='_'}*5})
