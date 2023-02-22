f7a7f576b2ec64c98928e238fa18c99c0f45eb41
Colabear754 <jky2406@gmail.com>
2023-02-22 21:26:08 +0900

    모독
    리팩토링

--- src/main/kotlin/_16600/16678.kt
+++ src/main/kotlin/_16600/16678.kt
@@ -4,10 +4,6 @@ fun main() = with(System.`in`.bufferedReader()) {
     val honor = IntArray(readLine().toInt()) { readLine().toInt() }.sortedArray()
     var target = 1
     var hackers = 0L
-    for (i in honor.indices) {
-        if (honor[i] < target) continue
-        hackers += honor[i] - target
-        honor[i] = target++
-    }
+    honor.map { if (it >= target) { hackers += it - target++ } }
     println(hackers)
 }
