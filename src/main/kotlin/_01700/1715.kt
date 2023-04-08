5bba8b1a1ce170ee0d1378c1a1c92adf9c6397db
Colabear754 <jky2406@gmail.com>
2023-04-08 11:55:26 +0900

    카드 정렬하기

--- src/main/kotlin/_01700/1715.kt
+++ src/main/kotlin/_01700/1715.kt
@@ -4,9 +4,7 @@ import java.util.PriorityQueue
 
 fun main() = with(System.`in`.bufferedReader()) {
     val cards = PriorityQueue<Int>()
-    repeat(readLine().toInt()) {
-        cards.add(readLine().toInt())
-    }
+    repeat(readLine().toInt()) { cards.add(readLine().toInt()) }
     var sum = 0
     while (cards.size > 1) sum += (cards.poll() + cards.poll()).also { cards.add(it) }
     println(sum)
