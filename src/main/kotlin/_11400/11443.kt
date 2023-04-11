df6d76008400353f994a600b0e5769e335b6fc0f
Colabear754 <jky2406@gmail.com>
2023-04-12 08:26:54 +0900

    짝수번째 피보나치 수의 합

---
+++ src/main/kotlin/_11400/11443.kt
@@ -0,0 +1,17 @@
+package _11400
+
+fun main() = print(readln().toLong().let { fibonacci(if (it % 2 == 0L) it + 1 else it) - 1  })
+
+private val memory = HashMap<Long, Long>().also {
+    it[0] = 0
+    it[1] = 1
+}
+
+private fun fibonacci(n: Long): Long {
+    return memory[n] ?: run {
+        val isEven = n % 2 == 0L
+        val f_n = fibonacci(n / 2)
+        val f_m = fibonacci(n / 2 + if (isEven) -1 else 1)
+        ((if (isEven) (f_n * (2 * f_m + f_n)) else (f_n * f_n + f_m * f_m)) % 1000000007).also { memory[n] = it }
+    }
+}
