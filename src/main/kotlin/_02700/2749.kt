06e8f6aa82e9bb4caa9f8c12cb8c4cb704a1aa1d
Colabear754 <jky2406@gmail.com>
2023-04-13 08:04:57 +0900

    피보나치 수 3

---
+++ src/main/kotlin/_02700/2749.kt
@@ -0,0 +1,17 @@
+package _02700
+
+fun main() = print(fibonacci(readln().toLong()))
+
+private val memory = HashMap<Long, Long>().also {
+    it[0] = 0
+    it[1] = 1
+}
+
+private fun fibonacci(n: Long): Long {
+    return memory[n] ?: run {
+        val isEven = n % 2 == 0L
+        val f_n = fibonacci(n / 2)
+        val f_m = fibonacci(n / 2 + if (isEven) -1 else 1)
+        ((if (isEven) (f_n * (2 * f_m + f_n)) else (f_n * f_n + f_m * f_m)) % 1000000).also { memory[n] = it }
+    }
+}
