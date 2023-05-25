f9192ac677446110d184c230ab7eba8216d13ba5
Colabear754 <jky2406@gmail.com>
2023-05-25 10:33:12 +0900

    조합

---
+++ src/main/kotlin/_02400/2407.kt
@@ -0,0 +1,16 @@
+package _02400
+
+import java.math.BigInteger
+
+fun main() = with(System.`in`.bufferedReader()) {
+    println(readLine().split(' ').map { it.toInt() }.let { comb(it[0], it[1]) })
+}
+
+fun comb(n: Int, k: Int): BigInteger {
+    var a = n
+    var result = BigInteger.ONE
+    var kFac = BigInteger.ONE
+    repeat(k) { result *= a--.toBigInteger() }
+    for (i in 2..k) kFac *= i.toBigInteger()
+    return result.divide(kFac)
+}
