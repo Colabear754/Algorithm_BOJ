4345f53044254824525599d23d4ab495d6181ab7
Colabear754 <jky2406@gmail.com>
2023-05-25 09:55:11 +0900

    RGB거리

---
+++ src/main/kotlin/_01100/1149.kt
@@ -0,0 +1,19 @@
+package _01100
+
+import java.util.StringTokenizer
+
+fun main() = with(System.`in`.bufferedReader()) {
+    val n = readLine().toInt()
+    val cost = arrayOf(IntArray(3)) + Array(n) { StringTokenizer(readLine()).run { IntArray(3) { nextToken().toInt() } } }
+    val dp = Array(n + 1) { IntArray(3) }
+    var min = 1000000000
+    repeat(3) { i ->
+        for (j in 1..n) {
+            dp[j][0] = minOf(dp[j - 1][1], dp[j - 1][2]) + cost[j][0]
+            dp[j][1] = minOf(dp[j - 1][0], dp[j - 1][2]) + cost[j][1]
+            dp[j][2] = minOf(dp[j - 1][0], dp[j - 1][1]) + cost[j][2]
+        }
+        repeat(3) { j -> if (i != j) min = minOf(min, dp[n][j]) }
+    }
+    println(min)
+}
