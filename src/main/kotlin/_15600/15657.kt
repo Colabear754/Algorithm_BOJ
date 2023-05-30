ba89db612ac261e7722982d26b41b5104736b993
Colabear754 <jky2406@gmail.com>
2023-05-30 12:41:19 +0900

    N과 M (8)

---
+++ src/main/kotlin/_15600/15657.kt
@@ -0,0 +1,23 @@
+package _15600
+
+import java.util.StringTokenizer
+
+fun main() = with(System.`in`.bufferedReader()) {
+    val bw = System.out.bufferedWriter()
+    val (n, m) = readLine().split(' ').map { it.toInt() }
+    val arr = StringTokenizer(readLine()).run { IntArray(n) { nextToken().toInt() }.sortedArray() }
+    val selected = IntArray(m)
+    fun dfs(depth: Int) {
+        if (depth == m) {
+            bw.write("${selected.joinToString(" ")}\n")
+            return
+        }
+        for (num in arr) {
+            if (depth > 0 && selected[depth - 1] > num) continue
+            selected[depth] = num
+            dfs(depth + 1)
+        }
+    }
+    dfs(0)
+    bw.close()
+}
