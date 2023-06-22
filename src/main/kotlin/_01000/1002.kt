844ff029752a8a57b3bb8c64bbfd6b48ff87364e
Colabear754 <jky2406@gmail.com>
2023-06-22 09:06:17 +0900

    터렛
    터진거 수정 및 리팩토링

--- src/main/kotlin/_01000/1002.kt
+++ src/main/kotlin/_01000/1002.kt
@@ -1,24 +1,26 @@
 package _01000
 
-import kotlin.math.*
+import java.util.StringTokenizer
 
 fun main() = with(System.`in`.bufferedReader()) {
     val bw = System.out.bufferedWriter()
     val n = readLine().toInt()
-    for (i in 0 until n) {
-        val input = readLine().split(" ").map { it.toDouble() }
+    repeat(n) {
+        val input = StringTokenizer(readLine()).run { LongArray(6) { nextToken().toLong() } }
         bw.write("${calc(input)}\n")
     }
     bw.close()
 }
 
-fun calc(l: List<Double>): Int {
-    val distance = sqrt((l[3] - l[0]).pow(2) + (l[4] - l[1]).pow(2))  // 두 점 사이의 거리
+private fun calc(l: LongArray): Int {
+    val distance = (l[3] - l[0]).pow() + (l[4] - l[1]).pow()
     val result = when {
-        distance == 0.0 && l[2] == l[5] -> -1   // 두 원이 일치하는 경우
-        distance > l[2] + l[5] || distance < abs(l[2] - l[5]) || (distance == 0.0 && l[2] != l[5]) -> 0 // 두 원이 만나지 않고 떨어져있거나 한 원 내부에 다른 원이 있는 경우
-        distance == (l[2] + l[5]) || distance == abs(l[2] - l[5]) -> 1    // 두 원이 한 점에서 접하는 경우
-        else -> 2   // 두 원이 두 점에서 접하는 경우
+        distance == 0L && l[2] == l[5] && l[2] != 0L -> -1
+        distance > (l[2] + l[5]).pow() || distance < (l[2] - l[5]).pow() || (distance == 0L && l[2] != l[5]) -> 0
+        distance == (l[2] + l[5]).pow() || distance == (l[2] - l[5]).pow() -> 1
+        else -> 2
     }
     return result
 }
+
+private fun Long.pow() = this * this
