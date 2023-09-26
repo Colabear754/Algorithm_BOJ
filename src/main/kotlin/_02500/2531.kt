851aaab91df1d6a4f5b3f231ece64d49aca51ffb
Colabear754 <jky2406@gmail.com>
2023-09-27 08:26:18 +0900

    회전 초밥

---
+++ src/main/kotlin/_02500/2531.kt
@@ -0,0 +1,25 @@
+package _02500
+
+fun main() = with(System.`in`.bufferedReader()) {
+    val (n, d, k, c) = readLine().split(" ").map { it.toInt() }
+    val check = IntArray(d + 1)
+    var count = 0
+    val sushi = IntArray(n) { i ->
+        readLine().toInt().also {
+            if (i < k) {
+                if (check[it] == 0) count++
+                check[it]++
+            }
+        }
+    }
+    var max = count
+    for (i in 0 until n) {
+        max = maxOf(max, if (check[c] == 0) count + 1 else count)
+        check[sushi[i]]--
+        if (check[sushi[i]] == 0) count--
+        val next = (i + k) % n
+        if (check[sushi[next]] == 0) count++
+        check[sushi[next]]++
+    }
+    println(max)
+}
