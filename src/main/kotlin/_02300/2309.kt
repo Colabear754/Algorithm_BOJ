1f4e89167a01da131bc3c0bc7a57327ffba7250d
Colabear754 <jky2406@gmail.com>
2024-01-27 17:58:52 +0900

    일곱 난쟁이

---
+++ src/main/kotlin/_02300/2309.kt
@@ -0,0 +1,14 @@
+package _02300
+
+fun main() = with(System.`in`.bufferedReader()) {
+    val dwarves = IntArray(9) { readLine().toInt() }.sortedArray()
+    val sum = dwarves.sum()
+    for (i in 0..<9) {
+        for (j in i + 1..<9) {
+            if (sum - dwarves[i] - dwarves[j] == 100) {
+                println(dwarves.filterIndexed { index, _ -> index != i && index != j }.joinToString("\n"))
+                return@with
+            }
+        }
+    }
+}
