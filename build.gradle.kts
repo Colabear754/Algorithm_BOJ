bc26e13725c7e366bf78ed89d5b57bc4a0c81d8a
Colabear754 <jky2406@gmail.com>
2024-04-03 08:24:38 +0900

    🎉 gradle 프로젝트로 변경

---
+++ build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    kotlin("jvm") version "1.9.10"
+}
+
+group = "com.colabear754"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(11)
+}
