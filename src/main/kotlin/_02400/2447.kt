1113b32fe2c0d50d2cbec3fb3468aff2f5088980
Colabear754 <jky2406@gmail.com>
2025-06-26 10:31:08 +0900

    별 찍기 - 10
    문제에서 사용한 변수, 함수 접근지정자 조정

--- src/main/kotlin/_02400/2447.kt
+++ src/main/kotlin/_02400/2447.kt
@@ -1,6 +1,6 @@
 package _02400
 
-val bw = System.out.bufferedWriter()
+private val bw = System.out.bufferedWriter()
 fun main() = with(System.`in`.bufferedReader()) {
     val n = readLine().toInt()
     repeat(n) { i ->
@@ -12,7 +12,7 @@ fun main() = with(System.`in`.bufferedReader()) {
     bw.close()
 }
 
-fun star(w: Int, h: Int, n: Int) {
+private fun star(w: Int, h: Int, n: Int) {
     when {
         n == 1 -> bw.write("*")
         ((w / (n / 3)) % 3) * ((h / (n / 3)) % 3) == 1 -> bw.write(" ")
