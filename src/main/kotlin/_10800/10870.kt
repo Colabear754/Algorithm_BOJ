10aa11b9ee45f72653e8a491a0c09c469cdd6863
Colabear754 <jky2406@gmail.com>
2025-06-26 10:35:14 +0900

    피보나치 수 5
    문제에서 사용한 함수 접근지정자 조정

--- src/main/kotlin/_10800/10870.kt
+++ src/main/kotlin/_10800/10870.kt
@@ -1,4 +1,4 @@
 package _10800
 
 fun main() = print(fibonacci(readln().toInt()))
-fun fibonacci(n: Int): Int = if (n <= 1) n else fibonacci(n - 1) + fibonacci(n - 2)
+private fun fibonacci(n: Int): Int = if (n <= 1) n else fibonacci(n - 1) + fibonacci(n - 2)
